a5dbb44cab25e5ce65476a0ebb40e78a63561c2a
Liza Degtyareva <liza.degt@yandex.ru>
2021-11-07 23:01:23 +0300

    README.md + test

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/k20bspj0mtr9pphp?svg=true)](https://ci.appveyor.com/project/lizadegt/cardorder)
---
+++ src/test/java/ru/netology/TestingTheCardApplicationForm.java
@@ -0,0 +1,23 @@
+package ru.netology;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestingTheCardApplicationForm {
+    @Test
+    void shouldRegisterByApplication() {
+        open("http://localhost:9999/");
+        SelenideElement form = $("[data-test-id=card-application-form]");
+        $("[name='name']").setValue("Иванов Иван");
+        $("[name='phone']").setValue("+79270000000");
+        $("[data-test-id='agreement']").click();
+        $("button").click();
+        $("[data-test-id='order-success']").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+
+}
